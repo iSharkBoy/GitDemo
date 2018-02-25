d52aa5f07bc53632ce35baea230afb09a37d2af9
gjwKongKong <523779838@qq.com>
2018-02-25 10:13:36 +0800

    first commit

---
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.test;
+
+import android.content.SyncStatusObserver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e("MainActivity","this is my githubDemo");
+    }
+}
